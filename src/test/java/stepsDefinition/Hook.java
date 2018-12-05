86cfacd1ad5586e038e8b22261dac06b334b99d2
YanaLeshkevich <yanaleshkevich@gmail.com>
2018-12-05 14:20:03 +0300

    reduced the waiting time for an item

--- src/test/java/stepsDefinition/Hook.java
+++ src/test/java/stepsDefinition/Hook.java
@@ -3,8 +3,13 @@ package stepsDefinition;
 import base.BaseUtil;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,16 +23,16 @@ public class Hook {
     @Before
     public void initializeTest(){
         System.out.println("Run Before Test!!!");
-        WebDriver driver = new ChromeDriver();
+        final WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         baseUtil.setDriver(driver);
     }
 
     @After
     public void tearDownTest(){
         System.out.println("Run after Test!!!");
-//        baseUtil.getDriver().quit();
+        baseUtil.getDriver().quit();
     }
 }
